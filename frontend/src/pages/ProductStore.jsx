5a4f18127820c7f206de7df63a4fc8bb0f6598b5
Saish Kocharekar <saishkocharekar@gmail.com>
2026-06-17 16:36:47 +0530

    pop up

--- frontend/src/pages/ProductStore.jsx
+++ frontend/src/pages/ProductStore.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { motion, AnimatePresence } from 'framer-motion';
 import { connectSocket } from '../services/socket';
-import { FaStar, FaShoppingCart, FaTrash, FaMinus, FaPlus } from 'react-icons/fa';
+import { FaStar, FaShoppingCart, FaTrash, FaMinus, FaPlus, FaCheckCircle } from 'react-icons/fa';
 import { HiX } from 'react-icons/hi';
 
 const ProductStore = () => {
